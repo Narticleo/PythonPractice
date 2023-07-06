8c81f73efa5757aab3a55ea64067f1505052bcfe
curry <curry871218@gmail.com>
2023-07-06 18:54:52 +0800

    number of senior citizens

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
-class A():
-    def add(self,a,b):
-        return a+b
-    def sub(self,a,b):
-        return a-b
-    def addsub(self,a,b,c):
-        return self.sub(self.add(a,b),c) 
-a = A()
-a.name = "abc"
-print(a.name)
-print(a.add(1,8))
-print(a.addsub(2,3,2))
+datas = ["0017","1299","1123"]
+age = [""*4 for row in range(3)]
+for i in range(0,len(datas)):
+    age[i] = datas[i][2:4]
+    print(int(age[i]))
+print(int(datas[0][0:2]))
