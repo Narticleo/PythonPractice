750895347ca3c20dabd3fe1a7b2e95452e33a9e1
narticleo <curry871218@gmail.com>
2023-07-06 18:49:15 +0800

    math.gcd

--- NumberOfBeautifulPairs.py
+++ NumberOfBeautifulPairs.py
@@ -1,15 +1,10 @@
-class Solution(object):
-    def gcd(self,m,n):
-        if n==0:
-            return m
-        else:
-            return self.gcd(n,m%n)
-        
+import math 
+class Solution(object):   
     def countBeautifulPairs(self, nums):
         pairs = 0
         for i in range(0,len(nums)):
             for j in range(i+1,len(nums)):
-                if self.gcd(int(str(nums[i])[0]),nums[j]%10) == 1:
+                if math.gcd(int(str(nums[i])[0]),nums[j]%10) == 1:
                     print(nums[i],nums[j])
                     pairs+=1
         return pairs
