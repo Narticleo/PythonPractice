0d3e205fdd8c1dac745da8f3448432480f0d4ace
narticleo <curry871218@gmail.com>
2023-07-09 00:42:53 +0800

    sprite manipulation

--- pythonGames/main.py
+++ pythonGames/main.py
@@ -17,18 +17,32 @@ class Player(pg.sprite.Sprite):
         self.image = pg.Surface((50,40))
         self.image.fill(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.rect.centerx = WIDTH/2
+        self.rect.centery = HEIGHT - self.rect.height/2 - 10
+        self.speedx = 10
+        self.speedy = 10
     def update(self):
-        if self.rect.left == WIDTH-1 or self.rect.left == WIDTH:
-            self.rect.right = 0
-        else:
-            self.rect.x+=2
-        if HEIGHT/2 - self.rect.top > self.rect.height/2:
-            self.rect.y+=10
-        elif HEIGHT/2 - self.rect.top < self.rect.height/2:
-            self.rect.y-=10
-        else:
-            self.rect.y+=5
+        key_pressed = pg.key.get_pressed()
+        
+        if key_pressed[pg.K_RIGHT] and self.rect.right < WIDTH:
+            self.rect.x += self.speedx
+        if key_pressed[pg.K_LEFT] and self.rect.left > 0:
+            self.rect.x -= self.speedx
+        if key_pressed[pg.K_UP] and self.rect.top > 0:
+            self.rect.y -= self.speedy
+        if key_pressed[pg.K_DOWN] and self.rect.bottom < HEIGHT:
+            self.rect.y += self.speedy
+
+        # if self.rect.left >= WIDTH:
+        #     self.rect.right = 0
+        # else:
+        #     self.rect.x+=2
+        # if HEIGHT/2 - self.rect.top > self.rect.height/2:
+        #     self.rect.y+=10
+        # elif HEIGHT/2 - self.rect.top < self.rect.height/2:
+        #     self.rect.y-=10
+        # else:
+        #     self.rect.y+=5
 
 all_sprite = pg.sprite.Group()
 player1 = Player()
