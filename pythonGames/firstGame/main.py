ad2de88272db7b6dcbbf43c33b833d48c387f624
narticleo <curry871218@gmail.com>
2023-07-10 16:42:51 +0800

    update

--- pythonGames/firstGame/main.py
+++ pythonGames/firstGame/main.py
@@ -7,30 +7,32 @@ SIZE = (WIDTH,HEIGHT)
 BLACK = (0,0,0)
 BROWN = (255,77,0)
 GOLDEN = (255,215,0)
-BACKGROUND = (30, 144, 255)
+WHITE = (246,246,246)
+BLUE = (30, 144, 255)
 FPS = 60
 
 pg.init()
 screen = pg.display.set_mode(SIZE)
-pg.display.set_caption("Battle Ships!")
+pg.display.set_caption("testing")
 #load imgs
 img_background = pg.image.load(os.path.join("imgs","background.jpg")).convert()
-img_ship = pg.image.load(os.path.join("imgs","ships.jpg")).convert()
+img_background = pg.transform.scale(img_background,(500,600))
+img_ship = pg.image.load(os.path.join("imgs","ship.jpg")).convert()
 img_meteorite = pg.image.load(os.path.join("imgs","meteorite.jpg")).convert()
-img_bullet = pg.image.load(os.path.join("imgs","bllet.png")).convert()
+img_bullet = pg.image.load(os.path.join("imgs","bullet.png")).convert()
 
 clock = pg.time.Clock()
 all_sprite = pg.sprite.Group()
 meteorites = pg.sprite.Group()
 bullets = pg.sprite.Group()
 
-
 #class Battleship
 class Battleship(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((50,40))
-        self.image.fill(BLACK)
+        self.image = img_ship
+        self.image = pg.transform.scale(self.image, (38,50))
+        self.image.set_colorkey(WHITE)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH/2
         self.rect.centery = HEIGHT - self.rect.height/2 - 10
@@ -56,7 +58,9 @@ class Bullet(pg.sprite.Sprite):
     def __init__(self,startx,starty):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface((8,15))
-        self.image.fill(GOLDEN)
+        self.image = img_bullet
+        self.image = pg.transform.scale(self.image,(40,30))
+        self.image.set_colorkey(WHITE)
         self.rect = self.image.get_rect()
         self.rect.centerx = startx
         self.rect.bottom = starty + 3
@@ -70,8 +74,11 @@ class Bullet(pg.sprite.Sprite):
 class Meteorite(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((50,40))
-        self.image.fill(BROWN)
+        # self.image = pg.Surface((50,40))
+        self.image = img_meteorite
+        self.p = rd.randrange(6,15)
+        self.image = pg.transform.scale(self.image,(5*self.p,4*self.p))
+        self.image.set_colorkey(WHITE)
         self.rect = self.image.get_rect()
         self.initializeParameter()
     def initializeParameter(self):
@@ -105,7 +112,7 @@ while running:
         if event.type == pg.QUIT:
             running = False
         elif event.type == pg.KEYDOWN:
-            if event.key == pg.K_LSHIFT:
+            if event.key == pg.K_LCTRL:
                 ship.shoot()
 
     collides = pg.sprite.groupcollide(bullets, meteorites, True, True)
@@ -114,12 +121,13 @@ while running:
         all_sprite.add(m)
         meteorites.add(m)
 
-    collides = pg.sprite.spritecollide(ship, meteorites, True)
-    if collides:
-        running = False
+    # collides = pg.sprite.spritecollide(ship, meteorites, True)
+    # if collides:
+    #     running = False
     
     all_sprite.update()
-    screen.fill(BACKGROUND)
+    # screen.fill(img_background)
+    screen.blit(img_background,(0,0))
     all_sprite.draw(screen)
     pg.display.update()
 
